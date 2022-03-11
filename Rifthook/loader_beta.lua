108fbd56563a923c243a2a3b907ae12b67d59465
Property <69339108+basedproperty@users.noreply.github.com>
2022-03-11 16:41:38 +0000

    Update loader_beta.lua

--- Rifthook/loader_beta.lua
+++ Rifthook/loader_beta.lua
@@ -1,2 +1,2 @@
 Release = "Beta"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/basedproperty/Rifthook/main/Rifthook/loader_beta.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/basedproperty/Rifthook/main/Rifthook/loader.lua"))()
