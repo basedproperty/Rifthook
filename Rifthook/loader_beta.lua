a1f924452917be0faf63026e9f589b9415e0a8d8
Property <69339108+basedproperty@users.noreply.github.com>
2022-04-05 16:50:08 +0100

    Update copyright

--- Rifthook/loader_beta.lua
+++ Rifthook/loader_beta.lua
@@ -1,2 +1,8 @@
+--[[
+    Copyright (C) 2020-2022 Rifthook Development Team
+    This file is related to RifthookLoader (also known as RLoad)
+
+    Under 2.a of the Rifthook Terms of Service, you are forbidden to reverse-engineer (or "deobfuscate") this script.
+]]
 Release = "Beta"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/basedproperty/Rifthook/main/Rifthook/loader.lua"))()
