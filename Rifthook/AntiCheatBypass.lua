6c6600bb88f8253298d7a7f720dda1e0d55520b7
Property <69339108+basedproperty@users.noreply.github.com>
2022-05-04 19:27:50 +0100

    Update AntiCheatBypass.lua

--- Rifthook/AntiCheatBypass.lua
+++ Rifthook/AntiCheatBypass.lua
@@ -3,13 +3,22 @@ local oldNameCall
 oldNameCall = hookmetamethod(game,"__namecall",function(self,...)
   local args = {...}
   local method = getnamecallmethod()
-  if not checkcaller() and getcallingscript().Name == "LocalClean" then -- localclean is retarded tbh (localclean causes those 1 sec lag spikes)
+  if not checkcaller() and (getcallingscript().Name == "LocalClean" or getcallingscript().Name == "CharacterControl") then -- localclean is retarded tbh (localclean causes those 1 sec lag spikes)
     return {}
   end
   if method == "FireServer" and self.Name == "RequestPlayerKick" then -- block humanoidcontrol requests for localplayer kick
     return "fortnite sussy balls :pensive:"
   end
+  
   return oldNameCall(self,...)
+end)
+
+local oldNewIndex
+oldNewIndex = hookmetamethod(game,"__newindex",function(self,key,value)
+   if not checkcaller() and (getcallingscript().Name == "CharacterControl") then -- localclean is retarded tbh (localclean causes those 1 sec lag spikes)
+    return {}
+  end
+  return oldNewIndex(self,key,value)
 end)
 while game:GetService("RunService").RenderStepped:Wait() do
   if LocalPlayer.Character and LocalPlayer.Character:FindFirstChild("CharacterControl") then -- not bothered to hook charactercontrol functions so just disable the script
